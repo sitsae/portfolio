53f83a6db7b7f1f3013f090cdafb97cb4eac40bb
Sigurd Torve Sæther <torve_s@live.no>
2023-10-14 16:00:19 +0200

    animated entrance of contact-spheres

--- resources/main.js
+++ resources/main.js
@@ -5,16 +5,33 @@ const emailButton = document.getElementsByClassName('hidden')[2];
 
 
 // console.log(emailButton)
-function unCollapseContactSpherse() {
-    if (discordButton.style.opacity === '0'){
-        discordButton.style.opacity = '100';
-        gitHubButton.style.opacity = '100';
-        emailButton.style.opacity = '100';
-    } else {
-        discordButton.style.opacity = '0';
-        gitHubButton.style.opacity = '0';
-        emailButton.style.opacity = '0';
-    }
-}
+// function unCollapseContactSpherse() {
+    
+//     if (discordButton.style.opacity === '0'){
+//         discordButton.style.opacity = '100';
+//         gitHubButton.style.opacity = '100';
+//         emailButton.style.opacity = '100';
+//     } else {
+//         discordButton.style.opacity = '0';
+//         gitHubButton.style.opacity = '0';
+//         emailButton.style.opacity = '0';
+//     }
+// }
 
-contactButton.onclick = unCollapseContactSpherse
+// contactButton.onclick = unCollapseContactSpherse
+// // contactButton.addEventListener('mouseover', unCollapseContactSpherse)
+
+//Koden over endrer opacity uten delay son funksjonen under.  
+
+function unCollapse() {
+    const elements = document.querySelectorAll(".small-sphere");
+
+    for (const element of elements) {
+      element.classList.toggle("small-sphere-transformed");
+
+    }
+  }
+  
+//   const changeButton = document.querySelector("#change");
+  contactButton.addEventListener("click", unCollapse);
+  
